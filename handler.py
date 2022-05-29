1576d6d71471a3ceb9d68d77eebeaed1fa54df72
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2022-05-29 18:51:20 +0900

    IAMで権限設定を追加

--- handler.py
+++ handler.py
@@ -1,16 +1,13 @@
-import json
-
+import datetime
+import boto3
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+  dt_now = datetime.datetime.now()
+  print('the object created at ' + str(dt_now) + '.') # 時刻を出力
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+  s3 = boto3.client('s3')
 
-    return response
+  response = s3.list_objects_v2(Bucket="photos20220120") # serverless.ymlで定義しているバケット名
 
+  for object in response['Contents']:
+      print(object['Key']) #S3バケットにあるオブジェクト名を出力する
